4cb2c61454241f7d2ad51dd3be4876172aaabdf0
William <william@williamlomas.me>
2018-11-03 16:31:37 -0700

    second progress

--- zeus.py
+++ zeus.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import mysql.connector
 import asyncio
 import json
+import random
 
 with open('config.json') as fp:
     data = json.load(fp)
@@ -13,6 +14,7 @@ bot = commands.AutoShardedBot(command_prefix='!', case_insensitive=True, activit
 bot.remove_command('help')
 
 lrs = []
+rs = []
 
 @bot.event
 async def on_message(x):
@@ -30,6 +32,8 @@ async def on_message(x):
         if x.channel.id == reportchannel and not x.content.startswith('!report'):
             await x.delete()
         else:
+            if x.author.id in rs:
+                return
             await bot.process_commands(x)
 
 @bot.command()
@@ -37,6 +41,7 @@ async def report(ctx):
     reporter = ctx.author
     cmdmsg = ctx.message
     lrs.append(reporter.id)
+    rs.append(reporter.id)
     if ctx.channel.id != reportchannel:
         return await ctx.send(f'{reporter.mention}, please send your request in <#{reportchannel}> instead.')
     await cmdmsg.delete()
@@ -52,16 +57,35 @@ async def report(ctx):
             await cmdmsg.delete()
             msg = await ctx.send(f'{reporter.mention}, since it has been over a minute without a response, this report has been closed automatically.\nYou can always open another with `!report`.')
             lrs.remove(reporter.id)
+            rs.remove(reporter.id)
             await asyncio.sleep(30)
             return await msg.delete()
-        data = msg.content
+        if message.startswith("P"):
+            data = ''
+            for attachment in msg.attachments:
+                filehash = random.getrandbits(64)
+                ext = attachment.filename.split('.')
+                ext.reverse()
+                ext = ext[0]
+                await attachment.save(fp=f'/var/www/i.williamlomas.me/zeusuploads/{filehash}.{ext}')
+                data = f'https://i.williamlomas.me/zeusuploads/{filehash}.{ext}'
+        else:
+            data = msg.content
+        if not data:
+            data = msg.content
         await cmdmsg.delete()
         await msg.delete()
         return data
 
     username = await requestinfo("What is the username of the person you are reporting?")
+    if not username:
+        return
     offenses = await requestinfo(f"What rules did `{username}` break?")
-    proof = await requestinfo(f"Please paste your proof of these offenses occuring in a link format.")
+    if not offenses:
+        return
+    proof = await requestinfo(f"Please upload proof of these offences happening. (link, or file upload)")
+    if not proof:
+        return
     cmdmsg = await ctx.send(f'{reporter.mention}\n\n**Do you have any additional comments? (React accordingly)**')
     lrs.remove(reporter.id)
     await cmdmsg.add_reaction('✅')
@@ -72,16 +96,44 @@ async def report(ctx):
         await cmdmsg.delete()
         msg = await ctx.send(f'{reporter.mention}, since it has been over a minute without a response, this report has been closed automatically.\nYou can always open another with `!report`.')
         lrs.remove(reporter.id)
+        rs.remove(reporter.id)
         await asyncio.sleep(30)
         return await msg.delete()
     if str(reaction[0].emoji) == '✅':
         lrs.append(reporter.id)
         await cmdmsg.delete()
         comments = await requestinfo("What are those additional comments?")
+        if not comments:
+            return
     else:
         await cmdmsg.delete()
         comments = 'None'
-    await ctx.send(username + '\n' + offenses + '\n' + proof + '\n' + comments) # debug stuff
-    lrs.remove(reporter.id)
+    try:
+        lrs.remove(reporter.id)
+    except: pass
+    cmdmsg = await ctx.send(f'{reporter.mention}, is the following information correct?\n\n**Username:** {username}\n**Offences:** {offenses}\n**Proof:** {proof}\n**Comments:** {comments}\n\nConfirmation here will send your report to the staff team.')
+    await cmdmsg.add_reaction('✅')
+    await cmdmsg.add_reaction('❌')
+    try:
+        reaction = await bot.wait_for('reaction_add', check=check2, timeout=60.0)
+    except asyncio.TimeoutError:
+        await cmdmsg.delete()
+        msg = await ctx.send(f'{reporter.mention}, since it has been over a minute without a response, this report has been closed automatically.\nYou can always open another with `!report`.')
+        try:
+            lrs.remove(reporter.id)
+        except: pass
+        rs.remove(reporter.id)
+        await asyncio.sleep(30)
+        return await msg.delete()
+    if str(reaction[0].emoji) == '✅':
+        await cmdmsg.delete()
+        try:
+            lrs.remove(reporter.id)
+        except: pass
+        rs.remove(reporter.id)
+        cmdmsg = await ctx.send(f'{reporter.mention}, thanks for your report!\nYou will receive updates about your report in a private message from me.')
+        await asyncio.sleep(20)
+        await cmdmsg.delete()
+
 
 bot.run(token)
